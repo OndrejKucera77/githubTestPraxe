0d9e78e8b83050145a252c9a2c39ccb79aee7a38
Ondra <o.kucera.st@spseiostrava.cz>
2023-05-25 13:47:26 +0200

    ES6 #2

--- es6.js
+++ es6.js
@@ -10,3 +10,16 @@ clovek.mluv();
 const mluveni = clovek.mluv.bind(clovek);
 console.log(mluveni);
 mluveni();
+
+//---------------------------------------------------
+const druhaMocnina = a => a*a;
+
+console.log(druhaMocnina(-4));
+
+const prace = [
+    {id: 1, aktivni: true},
+    {id: 2, aktivni: true},
+    {id: 3, aktivni: false}
+]
+
+aktivniPrace = prace.filter(pr => pr.aktivni)
